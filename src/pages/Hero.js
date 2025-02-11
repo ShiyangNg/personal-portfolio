192fef5908cd8830768f626c24cfcacdb372380e
Shi Yang Ng <ngshiyang@gmail.com>
2025-02-11 11:48:55 +0800

    .

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     
     <div className="p-24">
         <Reveal>
-        <div className="text-sky-100 font-bold font-montserrat text-7xl pb-4">Hey, I'm <span className="text-white">Shi Yang.</span></div>
+        <div className="text-sky-100 font-bold font-montserrat text-7xl pb-4">Hey, I'm <span className="text-white">Ng Shi Yang.</span></div>
         </Reveal>
       <Reveal>
       <div className="text-sky-200 font-montserrat text-2xl">I'm a <span className="text-white font-bold">Full Stack Developer</span></div>
